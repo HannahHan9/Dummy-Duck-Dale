2b20284b79b4abf172709f25c86fe6806a0a9c45
LucyRTCodes <lucyrtutton@gmail.com>
2023-07-28 14:20:03 +0100

    add form

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
 const PracticeScreen = () => {
 	return (
@@ -11,12 +13,38 @@ const PracticeScreen = () => {
 	);
 };
 
+const LoginScreen = () => {
+	const [name, setName] = useState("");
+	return (
+		<View style={styles.container}>
+			<Text>Register</Text>
+			<TextInput
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+				placeholder="Enter name"
+			/>
+			<TextInput title="Username" placeholder="Enter username" />
+			<TextInput
+				title="Password"
+				secureTextEntry={true}
+				placeholder="Enter password"
+			/>
+			<TextInput
+				title="Confirm password"
+				secureTextEntry={true}
+				placeholder="Confirm password"
+			/>
+		</View>
+	);
+};
+
 const Tab = createBottomTabNavigator();
 
 export const AppNavigator = () => {
 	return (
 		<Tab.Navigator>
 			<Tab.Screen name="Practice" component={PracticeScreen} />
+			<Tab.Screen name="Login" component={LoginScreen} />
 		</Tab.Navigator>
 	);
 };
