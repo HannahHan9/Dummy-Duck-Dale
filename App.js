14cb40bc5692bdd1c1b99ed5932364e59e8e6543
LucyRTCodes <lucyrtutton@gmail.com>
2023-07-28 11:26:54 +0100

    practcie navigation

--- App.js
+++ App.js
@@ -2,14 +2,30 @@ import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-	const [color, setColor] = useState("red");
+const PracticeScreen = () => {
 	return (
 		<View style={styles.container}>
 			<Text>Welcome to Duck Dale!</Text>
-			{/* <Button title="Hey Hannah" /> */}
 			<StatusBar style="auto" />
 		</View>
+	);
+};
+
+const Tab = createBottomTabNavigator();
+
+export const AppNavigator = () => {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name="Practice" component={PracticeScreen} />
+		</Tab.Navigator>
+	);
+};
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<AppNavigator />
+		</NavigationContainer>
 	);
 }
 
